c8b54bee9543275b614536094240cb2a76f13c30
Suang <Sukalst@qq.com>
2019-08-27 22:31:32 +0800

    Added seed

--- database/seeds/BookingSeeder.js
+++ database/seeds/BookingSeeder.js
@@ -12,9 +12,56 @@
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
+const Booking = use('App/Models/Booking')
 
 class BookingSeeder {
   async run () {
+    await Booking.creatMany(
+      [
+        {
+          start_date:'2019-01-01 12:00:00',
+          end_date: '2019-01-01 13:00:00',
+          status: 'Completed',
+          post_id: 1,
+          user_id: 3
+        },
+        {
+          start_date:'2019-08-11 12:00:00',
+          end_date: '2019-08-11 13:00:00',
+          status: 'Completed',
+          post_id: 1,
+          user_id: 6
+        },
+        {
+          start_date:'2019-07-01 12:00:00',
+          end_date: '2019-07-01 13:00:00',
+          status: 'Completed',
+          post_id: 2,
+          user_id: 3
+        },
+        {
+          start_date:'2019-09-01 12:00:00',
+          end_date: '2019-09-04 13:00:00',
+          status: 'Booked',
+          post_id: 4,
+          user_id: 5
+        },
+        {
+          start_date:'2019-10-01 12:00:00',
+          end_date: '2019-10-01 13:00:00',
+          status: 'Pending',
+          post_id: 5,
+          user_id: 6
+        },
+        {
+          start_date:'2019-09-01 12:00:00',
+          end_date: '2019-09-01 13:00:00',
+          status: 'Pending',
+          post_id: 1,
+          user_id: 3
+        }
+      ]
+    )
   }
 }
 
--- database/seeds/ReviewSeeder.js
+++ database/seeds/ReviewSeeder.js
@@ -12,9 +12,50 @@
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
+const Review = use('App/Models/Review')
 
 class ReviewSeeder {
   async run () {
+    await Review.creatMany(
+      [
+        {
+          star: 5,
+          comment: 'good',
+          user_id: 1,
+          reviwer_id: 2
+        },
+        {
+          star: 4,
+          comment: 'goodgood',
+          user_id: 1,
+          reviwer_id: 3
+        },
+        {
+          star: 3,
+          comment: 'goodgoodgood',
+          user_id: 4,
+          reviwer_id: 5
+        },
+        {
+          star: 2,
+          comment: 'goodgoodgoodgoodgood',
+          user_id: 6,
+          reviwer_id: 1
+        },
+        {
+          star: 5,
+          comment: 'goodgoodgoodgoodgood',
+          user_id: 2,
+          reviwer_id: 4
+        },
+        {
+          star: 1,
+          comment: 'goodgoodgoodgoodgoodgoodgood',
+          user_id: 3,
+          reviwer_id: 6
+        }
+      ]
+    )
   }
 }
 
