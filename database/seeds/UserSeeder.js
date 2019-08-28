4cb57245e3534c9ebe43099a96f0d477c7517dd2
Suang <Sukalst@qq.com>
2019-08-28 12:32:55 +0800

    updated seed

--- database/seeds/UserSeeder.js
+++ database/seeds/UserSeeder.js
@@ -19,7 +19,7 @@ class UserSeeder {
     await User.createMany(
       [
         {
-          username:'suang1',
+          // username:'suang1',
           email:'suang1@gmail.com',
           password:'suang123',
           firstname:'suang1',
@@ -27,7 +27,7 @@ class UserSeeder {
           profile_img:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png',
         },
         {
-          username:'suang2',
+          // username:'suang2',
           email:'suang2@gmail.com',
           password:'suang223',
           firstname:'suang2',
@@ -35,7 +35,7 @@ class UserSeeder {
           profile_img:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png',
         },
         {
-          username:'suang3',
+          // username:'suang3',
           email:'suang3@gmail.com',
           password:'suang323',
           firstname:'suang3',
@@ -43,7 +43,7 @@ class UserSeeder {
           profile_img:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png',
         },
         {
-          username:'suang4',
+          // username:'suang4',
           email:'suang4@gmail.com',
           password:'suang423',
           firstname:'suang4',
@@ -51,7 +51,7 @@ class UserSeeder {
           profile_img:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png',
         },
         {
-          username:'suang5',
+          // username:'suang5',
           email:'suang5@gmail.com',
           password:'suang523',
           firstname:'suang5',
@@ -59,7 +59,7 @@ class UserSeeder {
           profile_img:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png',
         },
         {
-          username:'suang6',
+          // username:'suang6',
           email:'suang6@gmail.com',
           password:'suang623',
           firstname:'suang6',
